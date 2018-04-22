708b0ba7960da1117b4fa747fef6047923c687db
Ivan Khud <anakin16@gmail.com>
2018-04-23 00:48:52 +0300

    ncdkjsvnkjenvjke

--- src/LeftSidebar.js
+++ src/LeftSidebar.js
@@ -1,15 +1,27 @@
 import React from 'react';
+import Meals from './Meals'
 
 export default class LeftSidebar extends React.Component {
+  
+  renderMealsList = () => {
+      return Meals.MealsList.map(meal => {
+        return <div class="category-body-item">{meal.name}</div>
+      })
+  }
+
   render () {
     return (
       <div className="sidebar sidebar-left">
-          aaaaaaaaaaaaaaaaaaaaaaaaaaa
+          <div class="search-container">
+              <input type="text" className="searchInput" placeholder="Type the name here" />
+              <i class="fa fa-search"></i>
+              <div class="search-result-container">
+                  <div class="search-response">
+                      {this.renderMealsList()}
+                  </div>
+              </div>
+          </div>
       </div>
     );
   }
 }
-
-
-
-
---
+++ src/Meals.js
@@ -0,0 +1,18 @@
+const MealsList = [
+
+    {name: 'Angus burger'},
+    {name: 'Australian burger'},
+    {name: 'Bacon cheeseburger'},
+    {name: 'Barbecue burger'},
+    {name: 'Butter burger'},
+    {name: 'Buffalo burger'},
+    {name: 'California burger'},
+    {name: 'Carolina burger'},
+    {name: 'Cheeseburger'},
+    {name: 'Chili burger'}
+    
+]
+
+export default {
+    MealsList
+}
